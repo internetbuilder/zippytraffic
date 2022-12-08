b587cd72ca1eb1ece7911e3e13acc07415db3083
Jordan Bourbonnais <45112383+internetbuilder@users.noreply.github.com>
2022-12-08 17:06:01 +0000

    ddd

--- main-about.js
+++ main-about.js
@@ -4,14 +4,23 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://zippytraffic.com/about.html" target="">
+    <a href="index.html" target="">
       <img src="/vite.svg" class="logo" alt="ZippyTraffic" />
     </a>
     
     <h1>ZippyTraffic!</h1>
+    
     <div class="card">
+    <h2>Traffic Exchange 1 for 1</h2>
+    
+    
+    <!--
+    
     <a href="https://bit.ly/zippytraffic" target="_blank">
+
     <button id="counter" type="button"></button>
+    
+    -->
   </a>
      
     </div>
